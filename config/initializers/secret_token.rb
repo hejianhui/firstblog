ce5fc8ccef417dddfff058221f53d965273df78f
hejianhui <314598029@qq.com>
2016-05-11 14:11:29 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstblog::Application.config.secret_key_base = 'a809b2e6feff76e8ae708a6c67093a6e6e830a256b918ab5fd9b52ca99d301cbb03ce7849443872d2f69f1bd3eb07888023941e7bff4f93f0f7f8d47f8296926'
