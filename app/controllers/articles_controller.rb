6b9e484642a8321f8a4af67c937e40c32f5b002a
hejianhui <314598029@qq.com>
2016-05-15 08:32:36 +0800

    改缩进

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,49 @@
 class ArticlesController < ApplicationController
-    before_action :signed_in_user, except: [:index, :show]
-    def new
-        @article = Article.new
-    end
+  before_action :signed_in_user, except: [:index, :show]
+  def new
+    @article = Article.new
+  end
 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-    def create
-        @article = current_user.articles.build(article_params)
+  def create
+    @article = current_user.articles.build(article_params)
 
-        if  @article.save
-            flash[:success] = "文章已创建"
-            redirect_to @article
-        else
-            render '/'
-        end
+    if  @article.save
+      flash[:success] = "文章已创建"
+      redirect_to @article
+    else
+      render '/'
     end
-    def update
-        @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
 
-        if @article.update(article_params)
-            redirect_to @article
-        else
-            render 'edit'
-        end
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-        redirect_to articles_path
-    end
+    redirect_to articles_path
+  end
 
-    private
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ActiveRecord::Base
-    belongs_to :user
-    has_many :comments, dependent: :destroy
-    validates :user_id, presence: true
-    #validates :content, presence: true
-    validates :title, presence: true,
-        length: { minimum: 5 }
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  validates :user_id, presence: true
+  #validates :content, presence: true
+  validates :title, presence: true,
+    length: { minimum: 5 }
 end
