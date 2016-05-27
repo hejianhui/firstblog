7539e38173b8e8f7e31d597def1e674006381d8c
hejianhui <314598029@qq.com>
2016-05-27 16:15:59 +0800

    改了下爬取的网页

--- crawl3.rb
+++ crawl3.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'mechanize'
 agent = Mechanize.new
-page = agent.get('http://zhihu.com')
+page = agent.get('https://www.zhihu.com/question/38270275')
 
 table = page.search('a')
 text = table.inner_text
